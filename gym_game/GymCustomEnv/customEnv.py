83094eafa3a7ba6b81e04948536b660ccf235190
Raphade <raphael-pennekamp@web.de>
2023-05-22 00:23:41 +0200

    pynput tests

--- gym_game/GymCustomEnv/customEnv.py
+++ gym_game/GymCustomEnv/customEnv.py
@@ -1,23 +1,35 @@
-import gym
+import subprocess
+from gym import Env
 from gym import spaces
 import numpy as np
 
-
-class CustomEnv(gym.Env):
+class CustomEnv(Env):
 
     def __init__(self, data_dict):
-
-
+        """initialize environment"""
+        # actions: left,right,up
+        self.action_space = spaces.Discrete(3)
+        # data we get from the game
         self.observation_space = {}
-        self.action_space = spaces.Discrete(9)
+        #self.game_cmd = ['C:\\Program Files\\LOVE\\love.exe', '..\\game']
+        #self.game_socket_rcv = ['python', '..\\python\\socketpy.py']
+        #self.game_process = subprocess.Popen(self.game_cmd)
+        #self.socket_process = subprocess.Popen(self.game_socket_rcv)
+        #self.observation_space = {}
 
-        self.data_dict = data_dict
+    def __delete__(self):
+        """initialize environment"""
+        self.game_process.kill()
+        self.socket_process.kill()
 
     def reset(self):
-        observation = 0 # still have to be define 
+        """reset environment"""
+        observation = 0 # still have to be define
         return observation
 
     def step(self, action):
+        """this function defines what to do in every step"""
+        self.Cu
         observation = 0
         reward = 0
         done = 0
@@ -25,3 +37,4 @@ class CustomEnv(gym.Env):
         return observation, reward, done, info
 
     #def render(self, mode='human'): 
+        """reset environment"""
