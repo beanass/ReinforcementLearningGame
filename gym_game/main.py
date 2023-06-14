a68536e1c1726292cb705f8a20be2ccdda409c28
ImanityDev <dupontm808@gmail.com>
2023-06-14 11:55:11 +0200

    Training data after 12 hours

--- gym_game/main.py
+++ gym_game/main.py
@@ -12,7 +12,7 @@ def main():
     space_shape = env.observation_space.shape
 
     agent = DQNAgent (space_shape[0]*space_shape[1]*space_shape[2], env.action_space.n)
-    #agent.load_model('model.pth')
+    agent.load_model('model.pth')
 
     state = env.reset()      
     done = False
